a02dde0e7a415c63c901b997e5c573eb6d12ce09
wgibbs3237 <wgibbs3237@gmail.com>
2024-06-27 12:07:42 +0100

    yobowebsite

---
+++ Homecarousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000, // Slide interval in milliseconds
+        autoplayHoverPause: true // Pause autoplay when hovering over the carousel
+    });
+});
